e4221d3f084070dd372410456cee7e8ce1263dce
Lluis <sarke56@gmail.com>
2019-10-10 09:52:12 +0200

    cambio en la key

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 SC.initialize( {
-    client_id: 'aa06b0630e34d6055f9c6f8beb8e02eb'
+    client_id: 'aa06b0630e34d6055f9c6f8beb8e02eb555'
 } );
 document.querySelector( '.buscarCancion' ).addEventListener( 'submit', function ( event ) {
     event.preventDefault();
