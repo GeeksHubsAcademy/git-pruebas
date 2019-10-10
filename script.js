df89345ba5235ca4834117f37c5cc44c6dbf94b4
DavidPinilla1 <d.pinilla@geekshubs.com>
2019-10-10 09:57:07 +0200

    asd

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 SC.initialize( {
-    client_id: 'aa06b0630e34d6055f9c6f8beb8e02eb'
+    client_id: 'adasd'
 } );
 document.querySelector( '.buscarCancion' ).addEventListener( 'submit', function ( event ) {
     event.preventDefault();
